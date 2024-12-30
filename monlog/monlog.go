9a6e4a19aa63add3f54b4cb98d548e312ca2ed4b
Maxim Temryakov <therevors@gmail.com>
2024-12-31 01:30:58 +0300

    feat(monlog.go): add initial code

--- monlog/monlog.go
+++ monlog/monlog.go
@@ -1,1 +1,32 @@
 package monlog
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	DEBUG = iota
+	TEST
+	PROD
+)
+
+type Logger struct {
+	prefix string
+	level  int
+	out    io.Writer
+}
+
+func NewLogger() *Logger {
+	return &Logger{
+		prefix: "monkey-logger",
+	}
+}
+
+func (l *Logger) Info(message string) {
+	timestamp := time.Now().Format(time.RFC3339)
+	log := fmt.Sprintf("%s [%s] %s: %s", l.prefix, timestamp, "DEBUG", message)
+	fmt.Fprintln(os.Stdout, log)
+}
